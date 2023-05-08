7c760a359f874055ae4900d92e4e8573f4a96a89
xcanwin <xcanwin@gmail.com>
2023-05-08 22:46:39 +0800

    建立项目

---
+++ KeepXFXingHuo.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name              KeepXFXingHuo
+// @description       沉浸式体验科大讯飞的星火认知大模型，支持国产，感觉讯飞星火体验还不错
+// @version           1.0
+// @author            xcanwin
+// @namespace         https://github.com/xcanwin/KeepXFXingHuo/
+// @supportURL        https://github.com/xcanwin/KeepXFXingHuo/
+// @updateURL         https://raw.githubusercontent.com/xcanwin/KeepXFXingHuo/main/KeepXFXingHuo.user.js
+// @downloadURL       https://raw.githubusercontent.com/xcanwin/KeepXFXingHuo/main/KeepXFXingHuo.user.js
+// @license           GPL-2.0-only
+// @match             https://xinghuo.xfyun.cn/desk
+// @run-at            document-idle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const $ = (Selector, el) => (el || document).querySelector(Selector);
+    const $$ = (Selector, el) => (el || document).querySelectorAll(Selector);
+
+    window.onload = function() {
+        setTimeout(function() {
+            $(".pages_switchTheme__2DNsU").lastElementChild.click();//切换沉浸主题
+            $(".pages_switchTheme__2DNsU").remove();//清理主题切换按钮
+            $(".pages_window_switch__J40yj").lastElementChild.click();//进入历史对话
+            $(".pages_window_switch__J40yj").firstElementChild.remove();//清理推荐对话
+            //清理水印//各位先留着吧，支持国产
+            $(".header_version_select__qCy40").remove();//清理顶部
+            $(".welcome-window_prompt_wrapper__nExUq").remove();//清理首页提示词指引
+            $(".affix_affix_content__-gEBT").parentElement.remove();//清理侧边栏
+            $(".chat-window_tip__-THKF").remove();//清理尾部
+        }, 1000);
+    };
+
+})();
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# KeepXFXingHuo
-沉浸式体验科大讯飞的星火认知大模型，支持国产，感觉讯飞星火体验还不错
+## 简介
+
+- 沉浸式体验科大讯飞的星火认知大模型，支持国产，感觉讯飞星火体验还不错
+
+## 安装渠道
+
+| 序号 | UserScript源 |
+| --- | --- |
+| 1 | [Github](https://raw.githubusercontent.com/xcanwin/KeepXFXingHuo/main/KeepXFXingHuo.user.js) |
+| 2 | [GreasyFork](https://greasyfork.org/zh-CN/scripts/465794-keepxfxinghuo) |
